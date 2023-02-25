874d1b110bdc62a7ffa10e714f91f30c52ccfbc1
WalobwaD <walobwadan@gmail.com>
2023-02-25 13:22:55 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import data from "./data.json"
+import DisplayData from './components/DisplayData';
+import { useEffect, useState } from 'react';
+function App() {
+  const [name, setName] = useState([])
+  useEffect(()=>{
+
+    const results = data.map((names)=>names.name)
+    console.log(results)
+    setName(results)
+  }, [])
+  return (
+    <div className="App">
+      <DisplayData names={name}/>
+    </div>
+  );
+}
+
+export default App;
