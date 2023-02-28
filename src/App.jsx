9f3b35c379fa2a66f2478a09527648cafe75a57a
Xtrialandtest <1trialandtest@gmail.com>
2023-02-28 21:20:42 +0300

    mush's branch first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import './App.css';
-import data from "./data.json"
+import data from './data.json';
 import DisplayData from './components/DisplayData';
 import { useEffect, useState } from 'react';
+import Mush from './components/mush';
+import mydata from './components/Mush.json';
 function App() {
-  const [name, setName] = useState([])
-  useEffect(()=>{
-
-    const results = data.map((names)=>names.name)
-    console.log(results)
-    setName(results)
-  }, [])
+  const [name, setName] = useState([]);
+  useEffect(() => {
+    const results = data.map((names) => names.name);
+    console.log(results);
+    setName(results);
+  }, []);
   return (
-    <div className="App">
-      <h1>Names of People</h1>
-      <DisplayData names={name}/>
-    </div>
+    <>
+      <div className='App'>
+        <h1>Names of People</h1>
+        <DisplayData names={name} />
+      </div>
+      <div className='mycontainer'>
+        <h1>the deadly squad</h1>
+        <div className='mush'>
+          {mydata.map((single) => {
+            return <Mush {...single} key={single.id} />;
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
